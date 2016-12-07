5fe7260345c4c390854d0d16e183f40f52fa46c0
tiskow <tybbow@hotmail.fr>
2016-12-07 07:53:50 -0800

    error fix libft

--- lib/ft_strncat.c
+++ lib/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: tiskow <tiskow@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/06 14:05:44 by tiskow            #+#    #+#             */
-/*   Updated: 2016/11/21 01:17:21 by tiskow           ###   ########.fr       */
+/*   Updated: 2016/12/05 12:42:42 by tiskow           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strncat(char *dest, const char *src, size_t n)
 	n = ft_min(n, ft_strlen(src) + 1);
 	len = ft_strlen(dest) + n;
 	ft_strncpy(&dest[ft_strlen(dest)], src, n);
-	if (len < ft_strlen(dest))
-		ft_strclr(&dest[len]);
+	if (len <= ft_strlen(dest))
+		ft_bzero(&dest[len], 1);
 	return (dest);
 }
